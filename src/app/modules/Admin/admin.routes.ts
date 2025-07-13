90a0e53b22ba967c0c4bc3928a6b7a3b50f389d2
Shafayathub <shafayat.ms@gmail.com>
2025-07-13 18:02:12 +0600

    auth layer added

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -2,14 +2,16 @@ import express from 'express';
 import { adminController } from './admin.controller';
 import { adminValidationSchemas } from './admin.validations';
 import validateRequest from '../../middlewares/ValidateRequest';
+import auth from '../../middlewares/Auth';
+import { UserRole } from '@prisma/client';
 
 const router = express.Router();
 
-router.get("/", adminController.getAllAdmin)
-router.get("/:id", adminController.getSingleAdmin) 
-router.patch("/:id",validateRequest(adminValidationSchemas.update), adminController.updateAdmin)
-router.delete("/:id", adminController.deleteAdmin)
-router.delete("/soft-delete/:id", adminController.softDeleteAdmin)
+router.get("/",auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminController.getAllAdmin);
+router.get("/:id", auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminController.getSingleAdmin) 
+router.patch("/:id",auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), validateRequest(adminValidationSchemas.update), adminController.updateAdmin)
+router.delete("/:id", auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminController.deleteAdmin)
+router.delete("/soft-delete/:id", auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), adminController.softDeleteAdmin)
 
 
 export const adminRoutes = router;
