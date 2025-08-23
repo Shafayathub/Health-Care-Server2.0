d043e59b02b5d76b9b287672e87c98e631451cba
Shafayathub <shafayat.ms@gmail.com>
2025-08-23 22:31:22 +0600

    zod required_error replaced

--- src/app/modules/Doctor/doctor.validation.ts
+++ src/app/modules/Doctor/doctor.validation.ts
@@ -3,37 +3,37 @@ import { z } from 'zod';
 const create = z.object({
     body: z.object({
         email: z.string({
-            required_error: 'Email is required',
+            error: 'Email is required',
         }),
         name: z.string({
-            required_error: 'Name is required',
+            error: 'Name is required',
         }),
         profilePhoto: z.string({
-            required_error: 'Profile Photo is required',
+            error: 'Profile Photo is required',
         }),
         contactNumber: z.string({
-            required_error: 'Contact Number is required',
+            error: 'Contact Number is required',
         }),
         registrationNumber: z.string({
-            required_error: 'Registration Number is required',
+            error: 'Registration Number is required',
         }),
         experience: z.number({
-            required_error: 'Experience is required',
+            error: 'Experience is required',
         }),
         gender: z.string({
-            required_error: 'Gender is required',
+            error: 'Gender is required',
         }),
         apointmentFee: z.number({
-            required_error: 'Blood group is required',
+            error: 'Blood group is required',
         }),
         qualification: z.string({
-            required_error: 'Apointment Fee is required',
+            error: 'Apointment Fee is required',
         }),
         currentWorkingPlace: z.string({
-            required_error: 'Current Working Place is required',
+            error: 'Current Working Place is required',
         }),
         designation: z.string({
-            required_error: 'Designation is required',
+            error: 'Designation is required',
         }),
     }),
 });
--- src/app/modules/Prescription/prescription.validation.ts
+++ src/app/modules/Prescription/prescription.validation.ts
@@ -3,10 +3,10 @@ import { z } from 'zod';
 const create = z.object({
     body: z.object({
         appointmentId: z.string({
-            required_error: 'Appointment Id is required',
+            error: 'Appointment Id is required',
         }),
         instructions: z.string({
-            required_error: 'Instructions is required',
+            error: 'Instructions is required',
         }),
     }),
 });
