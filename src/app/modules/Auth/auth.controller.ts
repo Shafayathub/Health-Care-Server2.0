88367b1c8ea5dd3f2cb8f51c15eff799f44507c0
Shafayathub <shafayat.ms@gmail.com>
2025-08-26 18:43:55 +0600

    cookie issue fixed

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -7,8 +7,12 @@ import httpStatus from "http-status";
 const loginUser = catchAsync(async (req: Request, res: Response) => {
     const result = await AuthServices.loginUser(req.body);
 
-    const { refreshToken } = result;
+    const { refreshToken, accessToken } = result;
 
+    res.cookie('accessToken', accessToken, {
+        secure: false,
+        httpOnly: true
+    })
     res.cookie('refreshToken', refreshToken, {
         secure: false,
         httpOnly: true
@@ -19,7 +23,6 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
         success: true,
         message: "Logged in successfully!",
         data: {
-            accessToken: result.accessToken,
             needPasswordChange: result.needPasswordChange
         }
     })
@@ -29,6 +32,10 @@ const refreshToken = catchAsync(async (req: Request, res: Response) => {
     const { refreshToken } = req.cookies;
 
     const result = await AuthServices.refreshToken(refreshToken);
+    res.cookie('accessToken', result.accessToken, {
+        secure: false,
+        httpOnly: true
+    })
 
     sendResponse(res, {
         statusCode: httpStatus.OK,
